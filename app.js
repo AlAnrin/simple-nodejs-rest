ca9a4c0ccb466f65b885b0da3d3597d1497cefd3
AlAnrin <lianirvi@gmail.com>
2020-11-10 10:48:20 +0300

    delete host

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.post('/api/auth', (req, res) => {
 
 app.use('/api', routes);
 
-const server = app.listen(process.env.PORT || port, host, () => console.log(`Server listens http://${host}:${port}`));
+const server = app.listen(process.env.PORT || port, () => console.log(`Server listens ${port}`));
 
 server.keepAliveTimeout = 30000;
